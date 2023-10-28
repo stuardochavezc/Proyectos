ccdc757a3c04218021f6d124327bb3a0b8c17f1f
estuardochavezc <mchavezc19@miumg.edu.gt>
2023-10-28 08:46:25 -0600

    first commit

---
+++ HolaMiInge/src/holamiinge/HolaMiInge.java
@@ -0,0 +1,19 @@
+
+package holamiinge;
+
+/**
+ *
+ * @author umg
+ */
+public class HolaMiInge {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+     
+        System.out.println("Mi inge el proyecto de juego del parchis esta muy complicado hechenos la mano. ");
+    }
+    
+}
